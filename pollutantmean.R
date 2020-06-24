cf03d2e747cd350ffc93f812737e89d609e76cc9
BichTran91 <bich.t.tran.91@gmail.com>
2020-06-24 13:13:37 -0400

    wrote pollutantmean function

---
+++ pollutantmean.R
@@ -0,0 +1,11 @@
+pollutantmean<- function(directory, pollutant, id = 1:332) {
+        files<- list.files(directory, full.names = TRUE)
+        data<- data.frame()
+        for (i in id) {
+                data<- rbind(data, read.csv(files[i]))
+        }
+        outcome<- mean(data[, pollutant], na.rm = TRUE)
+        return(outcome)
+}
+
+pollutantmean("specdata", "sulfate", 1:332)
